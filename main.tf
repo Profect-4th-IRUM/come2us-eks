c52a143f9b0e24f6c232b650196c31d4fd96ab41
Bal1oon <ws32159@naver.com>
2026-03-14 00:45:00 +0900

    refactor #8: 미사용 provider 제거

--- main.tf
+++ main.tf
@@ -4,16 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.95"
     }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.20.0"
-    }
-
-    utils = {
-      source  = "cloudposse/utils"
-      version = ">= 0.17.0"
-    }
   }
 
   backend "s3" {}
--- provider.tf
+++ provider.tf
@@ -7,23 +7,3 @@ provider "aws" {
     role_arn = var.terraform_role_arn
   }
 }
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-
-provider "helm" {
-  kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
