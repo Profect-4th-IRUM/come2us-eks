e0f359ef88487ecbc440a3c25ea0a0e959b85dd5
Bal1oon <ws32159@naver.com>
2026-03-17 18:08:47 +0900

    fix #8: 정책 수정 '/ -> :'

--- iam.tf
+++ iam.tf
@@ -16,7 +16,7 @@ module "alb_controller_irsa" {
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system/aws-load-balancer-controller"]
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
     }
   }
 }
