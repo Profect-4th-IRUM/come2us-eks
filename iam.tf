3fb11df14e5e25b538641a016d760b8d67e5e3d1
Bal1oon <ws32159@naver.com>
2026-03-18 22:13:09 +0900

    feat #8: IRSA 추가

--- iam.tf
+++ iam.tf
@@ -20,3 +20,19 @@ module "alb_controller_irsa" {
     }
   }
 }
+
+module "external_dns_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name                     = "${var.prefix}-external-dns"
+  attach_external_dns_policy    = true
+  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${data.terraform_remote_state.dns.outputs.zone_id}"]
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+}
