cc17dfb1bf8edc0b5ba055742603a65146afb704
Bal1oon <ws32159@naver.com>
2026-03-24 17:31:00 +0900

    feat #8: ExternalSecret IRSA 추가

--- iam.tf
+++ iam.tf
@@ -36,3 +36,19 @@ module "external_dns_irsa" {
     }
   }
 }
+
+module "external_secrets_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name                             = "${var.prefix}-external-secrets"
+  attach_external_secrets_policy        = true
+  external_secrets_ssm_parameter_arns   = ["arn:aws:ssm:ap-northeast-2:${var.account_id}:parameter/${var.prefix}/*"]
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["external-secrets:external-secrets"]
+    }
+  }
+}
